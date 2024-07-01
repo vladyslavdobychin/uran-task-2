e45d7c123c8c0ddd1b913b490b571cedb7a29454
VladDobychin <vladyslav.dobychin@binary-studio.com>
2024-07-01 12:52:41 +0200

    Implementing-CRUD: added routes for interacting with products and product categories

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ProductCategoryController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -15,6 +17,10 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    // Add your new routes for products and product categories here
+    Route::resource('products', ProductController::class);
+    Route::resource('product-categories', ProductCategoryController::class);
 });
 
 require __DIR__.'/auth.php';
