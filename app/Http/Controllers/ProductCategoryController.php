aafd459d74a46ffb8e0d7dcff4a4aa873fb337bd
VladDobychin <vladyslav.dobychin@binary-studio.com>
2024-07-01 12:57:28 +0200

    Implementing-CRUD: setup product category controller

--- app/Http/Controllers/ProductCategoryController.php
+++ app/Http/Controllers/ProductCategoryController.php
@@ -2,63 +2,57 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\ProductCategory;
 use Illuminate\Http\Request;
 
 class ProductCategoryController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
-        //
+        $categories = ProductCategory::all();
+        return view('product-categories.index', compact('categories'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
     public function create()
     {
-        //
+        return view('product-categories.create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'name' => 'required',
+        ]);
+
+        ProductCategory::create($request->all());
+
+        return redirect()->route('product-categories.index')->with('success', 'Product category created successfully.');
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
+    public function show(ProductCategory $productCategory)
     {
-        //
+        return view('product-categories.show', compact('productCategory'));
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
+    public function edit(ProductCategory $productCategory)
     {
-        //
+        return view('product-categories.edit', compact('productCategory'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
+    public function update(Request $request, ProductCategory $productCategory)
     {
-        //
+        $request->validate([
+            'name' => 'required',
+        ]);
+
+        $productCategory->update($request->all());
+
+        return redirect()->route('product-categories.index')->with('success', 'Product category updated successfully.');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
+    public function destroy(ProductCategory $productCategory)
     {
-        //
+        $productCategory->delete();
+        return redirect()->route('product-categories.index')->with('success', 'Product category deleted successfully.');
     }
 }
