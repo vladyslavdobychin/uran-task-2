e841fc5b24921c9d05296160dd3f06a0dd8f221b
VladDobychin <vladyslav.dobychin@binary-studio.com>
2024-07-01 13:25:57 +0200

    Implementing-CRUD: made sure properties are writable

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['name', 'description', 'price', 'product_category_id'];
+
+    public function category()
+    {
+        return $this->belongsTo(ProductCategory::class, 'product_category_id');
+    }
 }
